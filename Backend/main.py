b46991487e3749a3dfe37dc2a7b25a779f611974
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-11-19 23:35:37 +0530

    Delete a Task

--- Backend/main.py
+++ Backend/main.py
@@ -4,6 +4,7 @@ import models
 
 from database import Base, engine, SessionLocal
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
 
 # create database if not created
 Base.metadata.create_all(engine)
@@ -26,11 +27,25 @@ def getTasks(session : Session = Depends(get_session)):
 
 # Add a Task
 @app.post('/')
-def addTasks(task : schemas.Tasks, session : Session = Depends(get_session)):
+def addTask(task : schemas.Tasks, session : Session = Depends(get_session)):
     taskObject = models.Tasks(current_task = task.current_task)
     session.add(taskObject)
     session.commit()
     session.refresh(taskObject)
     return taskObject   
+
+#Delete a Task
+@app.delete('/{id}')
+def deleteTask(id : int, session : Session = Depends(get_session)):
+    try:
+        itemObject = session.query(models.Tasks).get(id)
+        session.delete(itemObject)
+        session.commit()
+        session.close()
+        return 'Task has been deleted'
+    
+    except SQLAlchemyError as e:
+        return 'Please enter correct task id'
+
 
 
