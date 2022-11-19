c6f972ac2814fc1e197463c5463e81793c57f6c5
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-11-19 23:26:33 +0530

    Add A Task

--- Backend/main.py
+++ Backend/main.py
@@ -19,9 +19,18 @@ def get_session():
 app = FastAPI()
 
 # Get All Tasks
-@app.get("/")
+@app.get('/')
 def getTasks(session : Session = Depends(get_session)):
     taskObject = session.query(models.Tasks).all()
     return taskObject
+
+# Add a Task
+@app.post('/')
+def addTasks(task : schemas.Tasks, session : Session = Depends(get_session)):
+    taskObject = models.Tasks(current_task = task.current_task)
+    session.add(taskObject)
+    session.commit()
+    session.refresh(taskObject)
+    return taskObject   
 
 
